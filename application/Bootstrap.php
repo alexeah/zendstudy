1477b20aefaf62cbb61bf9b07934d0d3fffb8e37
vershilo <mk39aouyt2@vershilo.vershilo>
2012-11-18 23:17:05 +0300

    ZFDebug

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,10 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    public function __construct($application)
+    {
+        require_once('ZFDebug/Debug.php');
+        Zend_Controller_Front::getInstance()->registerPlugin(new ZFDebug_Controller_Plugin_Debug());
+        parent::__construct($application);
+    }
 }
-
